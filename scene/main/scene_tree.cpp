42af4d50e696e790e33a0d9347fbe55e06da13e8
Yukitty <catgirl@goddess.moe>
2020-10-14 13:57:28 -0400

    Added ProjectSetting display/window/stretch/integer_scale
    This bool changes stretch behavior to snap to the highest available
    whole-integer scale of the desired resolution, and either letterbox
    or expand the aspect ratio along both axes simultaneously.
    
    This behavior is best suited for scaling up pixel art games from an
    extreme low base resolution using STRETCH_MODE_2D.

--- scene/main/scene_tree.cpp
+++ scene/main/scene_tree.cpp
@@ -1145,6 +1145,8 @@ void SceneTree::_update_root_rect() {
 
 	Size2 viewport_size;
 	Size2 screen_size;
+	Size2 expand_size;
+	unsigned scale = 1;
 
 	float viewport_aspect = desired_res.aspect();
 	float video_mode_aspect = video_mode.aspect();
@@ -1153,40 +1155,76 @@ void SceneTree::_update_root_rect() {
 		WARN_PRINT("Font oversampling only works with the resize modes 'Keep Width', 'Keep Height', and 'Expand'.");
 	}
 
+	if (stretch_integer) {
+		scale = (unsigned)fmax(floor(fmin(video_mode.x / desired_res.x, video_mode.y / desired_res.y)), 1.0);
+		expand_size = (video_mode / scale).floor() - desired_res;
+	}
+
 	if (stretch_aspect == STRETCH_ASPECT_IGNORE || Math::is_equal_approx(viewport_aspect, video_mode_aspect)) {
 		//same aspect or ignore aspect
 		viewport_size = desired_res;
 		screen_size = video_mode;
+	} else if (stretch_aspect == STRETCH_ASPECT_EXPAND && stretch_integer) {
+		// Stretch horizontally AND vertically.
+		viewport_size = desired_res + expand_size;
+		screen_size = viewport_size * scale;
 	} else if (viewport_aspect < video_mode_aspect) {
 		// screen ratio is smaller vertically
 
 		if (stretch_aspect == STRETCH_ASPECT_KEEP_HEIGHT || stretch_aspect == STRETCH_ASPECT_EXPAND) {
 
 			//will stretch horizontally
-			viewport_size.x = desired_res.y * video_mode_aspect;
-			viewport_size.y = desired_res.y;
-			screen_size = video_mode;
+			if (stretch_integer) {
+				viewport_size.x = desired_res.x + expand_size.x;
+				viewport_size.y = desired_res.y;
+				screen_size.x = viewport_size.x;
+				screen_size.y = desired_res.y;
+				screen_size *= scale;
+
+			} else {
+				viewport_size.x = desired_res.y * video_mode_aspect;
+				viewport_size.y = desired_res.y;
+				screen_size = video_mode;
+			}
 
 		} else {
-			//will need black bars
+			//will need side bars
 			viewport_size = desired_res;
-			screen_size.x = video_mode.y * viewport_aspect;
-			screen_size.y = video_mode.y;
+			if (stretch_integer) {
+				// attempt a pixel-perfect render
+				screen_size = desired_res * scale;
+			} else {
+				screen_size.x = video_mode.y * viewport_aspect;
+				screen_size.y = video_mode.y;
+			}
 		}
 	} else {
 		//screen ratio is smaller horizontally
 		if (stretch_aspect == STRETCH_ASPECT_KEEP_WIDTH || stretch_aspect == STRETCH_ASPECT_EXPAND) {
 
-			//will stretch horizontally
-			viewport_size.x = desired_res.x;
-			viewport_size.y = desired_res.x / video_mode_aspect;
-			screen_size = video_mode;
+			//will stretch vertically
+			if (stretch_integer) {
+				viewport_size.x = desired_res.x;
+				viewport_size.y = desired_res.y + expand_size.y;
+				screen_size.x = desired_res.x;
+				screen_size.y = viewport_size.y;
+				screen_size *= scale;
+			} else {
+				viewport_size.x = desired_res.x;
+				viewport_size.y = desired_res.x / video_mode_aspect;
+				screen_size = video_mode;
+			}
 
 		} else {
-			//will need black bars
+			//will need a letterbox
 			viewport_size = desired_res;
-			screen_size.x = video_mode.x;
-			screen_size.y = video_mode.x / viewport_aspect;
+			if (stretch_integer) {
+				// attempt a pixel-perfect render
+				screen_size = desired_res * scale;
+			} else {
+				screen_size.x = video_mode.x;
+				screen_size.y = video_mode.x / viewport_aspect;
+			}
 		}
 	}
 
@@ -1194,19 +1232,14 @@ void SceneTree::_update_root_rect() {
 	viewport_size = viewport_size.floor();
 
 	Size2 margin;
-	Size2 offset;
 	//black bars and margin
-	if (stretch_aspect != STRETCH_ASPECT_EXPAND && screen_size.x < video_mode.x) {
+	if (screen_size.x < video_mode.x) {
 		margin.x = Math::round((video_mode.x - screen_size.x) / 2.0);
-		VisualServer::get_singleton()->black_bars_set_margins(margin.x, 0, margin.x, 0);
-		offset.x = Math::round(margin.x * viewport_size.y / screen_size.y);
-	} else if (stretch_aspect != STRETCH_ASPECT_EXPAND && screen_size.y < video_mode.y) {
-		margin.y = Math::round((video_mode.y - screen_size.y) / 2.0);
-		VisualServer::get_singleton()->black_bars_set_margins(0, margin.y, 0, margin.y);
-		offset.y = Math::round(margin.y * viewport_size.x / screen_size.x);
-	} else {
-		VisualServer::get_singleton()->black_bars_set_margins(0, 0, 0, 0);
 	}
+	if (screen_size.y < video_mode.y) {
+		margin.y = Math::round((video_mode.y - screen_size.y) / 2.0);
+	}
+	VisualServer::get_singleton()->black_bars_set_margins(margin.x, margin.y, margin.x, margin.y);
 
 	switch (stretch_mode) {
 		case STRETCH_MODE_DISABLED: {
@@ -1215,7 +1248,7 @@ void SceneTree::_update_root_rect() {
 		} break;
 		case STRETCH_MODE_2D: {
 
-			_update_font_oversampling(screen_size.x / viewport_size.x); //screen / viewport radio drives oversampling
+			_update_font_oversampling(fmax(screen_size.x / viewport_size.x, screen_size.y / viewport_size.y)); // screen / viewport ratio drives oversampling
 			root->set_size((screen_size / stretch_shrink).floor());
 			root->set_attach_to_screen_rect(Rect2(margin, screen_size));
 			root->set_size_override_stretch(true);
@@ -1240,12 +1273,13 @@ void SceneTree::_update_root_rect() {
 	}
 }
 
-void SceneTree::set_screen_stretch(StretchMode p_mode, StretchAspect p_aspect, const Size2 &p_minsize, real_t p_shrink) {
+void SceneTree::set_screen_stretch(StretchMode p_mode, StretchAspect p_aspect, const Size2 &p_minsize, real_t p_shrink, bool p_integer_scale) {
 
 	stretch_mode = p_mode;
 	stretch_aspect = p_aspect;
 	stretch_min = p_minsize;
 	stretch_shrink = p_shrink;
+	stretch_integer = p_integer_scale;
 	_update_root_rect();
 }
 
